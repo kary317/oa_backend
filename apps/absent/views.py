6a2af831e56eec212839bdb41a6643c8b8d408f2
kary <kary.sun@outlook.com>
2025-11-28 21:24:51 +0800

    考勤视图集和序列化功能(5) - 视图集重写list方法,查看用户考勤和下属考勤

--- apps/absent/views.py
+++ apps/absent/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import render
 
 from rest_framework import viewsets
 from rest_framework import mixins
+from rest_framework.response import Response
 
 from .models import Absent, AbsentType, AbsentStatusChoices
 from .serializers import AbsentSerializer
@@ -16,7 +17,7 @@ class AbsentViewSet(mixins.CreateModelMixin,
                     mixins.UpdateModelMixin,
                     mixins.ListModelMixin,
                     viewsets.GenericViewSet):
-    queryset = Absent
+    queryset = Absent.objects.all()
     serializer_class = AbsentSerializer
 
     def update(self, request, *args, **kwargs):
@@ -25,3 +26,13 @@ class AbsentViewSet(mixins.CreateModelMixin,
         # DRF 序列化器默认要求全字段提交与验证。通过设置 partial=True 可开启部分更新模式，此时仅验证所提交的字段。
         kwargs['partial'] = True
         return super().update(request, *args, **kwargs)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        who = request.query_params.get('who')
+        if who and who == 'sub':
+            result = queryset.filter(responder=request.user)
+        else:
+            result = queryset.filter(requester=request.user)
+        serializer = self.serializer_class(result, many=True)
+        return Response(serializer.data)
