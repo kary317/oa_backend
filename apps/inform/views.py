e4d14763ca9b8933d47190afa3d9776d6ada3ec3
kary <kary.sun@outlook.com>
2025-12-09 14:20:26 +0800

    解决小bug,通知列表去重处理

--- apps/inform/views.py
+++ apps/inform/views.py
@@ -17,7 +17,7 @@ class InformViewSet(viewsets.ModelViewSet):
     # 3. inform.author = request.user 通知的作者是当前登录用户
     def get_queryset(self):
         queryset = self.queryset.select_related('author').prefetch_related('reads', 'departments').filter(
-            Q(public=True) | Q(departments=self.request.user.department) | Q(author=self.request.user))
+            Q(public=True) | Q(departments=self.request.user.department) | Q(author=self.request.user)).distinct()
         return queryset
         # for inform in queryset:
         #     inform.is_read = InformRead.objects.filter(inform=inform, user=self.request.user).exsits()
