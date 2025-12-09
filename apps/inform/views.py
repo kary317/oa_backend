f3173da4c7d5bdcdab5c363d38ba3c9db2827f31
kary <kary.sun@outlook.com>
2025-12-09 12:36:28 +0800

    重写destroy方法删除通知 只允许删除当前登录用户本人的

--- apps/inform/views.py
+++ apps/inform/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
 from django.db.models import Q
+from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Inform
 from .serializers import InformSerializer
@@ -14,9 +16,18 @@ class InformViewSet(viewsets.ModelViewSet):
     # 2. inform.departments 包含了用户所在的部门
     # 3. inform.author = request.user 通知的作者是当前登录用户
     def get_queryset(self):
-        queryset = self.queryset.prefetch_related('reads').filter(
+        queryset = self.queryset.select_related('author').prefetch_related('reads', 'departments').filter(
             Q(public=True) | Q(departments=self.request.user.department) | Q(author=self.request.user))
         return queryset
         # for inform in queryset:
         #     inform.is_read = InformRead.objects.filter(inform=inform, user=self.request.user).exsits()
         # return queryset
+
+    # 只允许删除当前登录用户本人的
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.author.uid == request.user.uid:
+            self.perform_destroy(instance)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
