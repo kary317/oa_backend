272d8cc5261c04bc3ed767a83ecaae395669cb49
kary <kary.sun@outlook.com>
2025-12-10 15:17:55 +0800

    引入AES加密 发送激活员工邮件

--- apps/staff/views.py
+++ apps/staff/views.py
@@ -9,8 +9,11 @@ from django.conf import settings
 from apps.oaauth.models import OADepartment
 from apps.oaauth.serializers import DepartmentSerializer
 from .serializers import AddStaffSerializer
+from utils import aeser
+from django.urls import reverse
 
 OAUser = get_user_model()
+aes = aeser.AESCipher(settings.SECRET_KEY)
 
 
 class DepartmentListView(ListAPIView):
@@ -18,7 +21,18 @@ class DepartmentListView(ListAPIView):
     serializer_class = DepartmentSerializer
 
 
+# 激活员工邮箱
+class ActiveStaffView(APIView):
+    def get(self, request):
+        return Response()
+
+
 class StaffView(APIView):
+    # 获取员工列表
+    def get(self, request):
+        pass
+
+    # 新增员工
     def post(self, request):
         # 如果用的是视图集，那么视图集会自动把request放到context中
         # 如果是直接继承自APIView，那么就需要手动将request对象传给serializer.context中
@@ -31,12 +45,30 @@ class StaffView(APIView):
             # user = OAUser.objects.create(email=email, realname=realname)
             # user.set_password(password)
             # user.save()
+            # 1. 保存用户数据
             user = OAUser.objects.create_user(email=email, realname=realname, password=password)
             department = request.user.department
             user.department = department
             user.save()
 
-            send_mail('OA员工激活', message='xxx', from_email=settings.DEFAULT_FROM_EMAIL, recipient_list=[email])
+            # 2.发送激活邮件
+            self.send_active_email(email)
+
             return Response()
         else:
             return Response({'detail': list(serializer.errors.values())[0][0]}, status=status.HTTP_400_BAD_REQUEST)
+
+    # 发送激活邮件
+    def send_active_email(self, email):
+        token = aes.encrypt(email)
+
+        # '/staff/active/?token='xxx'
+        active_path = reverse('staff:active_staff') + '?token=' + token
+        # 通过self.request.build_absolute_uri构建绝对路径url ==> http://127.0.0.1:8000/staff/active/?token='xxx'
+        active_url = self.request.build_absolute_uri(active_path)
+
+        message = f'请点击以下链接激活账号：{active_url}'
+        # 发送一个链接，让用户点击这个链接后，跳转到激活的页面，才能激活。
+        # 为了区分用户，在发送链接邮件中，该链接中应该要包含这个用户的邮箱
+        # 针对邮箱要进行加密：AES加密
+        send_mail('OA员工激活', message=message, from_email=settings.DEFAULT_FROM_EMAIL, recipient_list=[email])
